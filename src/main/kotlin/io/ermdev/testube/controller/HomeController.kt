4ef1b5c9a19c80151343fd1664d3d5ddebb821bf
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-22 00:32:13 +0800

    Remove the shell and bat files

--- src/main/kotlin/io/ermdev/testube/controller/HomeController.kt
+++ src/main/kotlin/io/ermdev/testube/controller/HomeController.kt
@@ -1,4 +1,13 @@
 package io.ermdev.testube.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class HomeController {
+
+    @GetMapping
+    fun showHome(): String {
+        return "home"
+    }
 }
